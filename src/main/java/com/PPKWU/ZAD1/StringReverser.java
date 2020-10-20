455bdd475e30612521726909557a59b01be21c6d
Tomasz Kucharczyk <215786@edu.p.lodz.pl>
2020-10-20 14:41:34 +0200

    Added RequestParam annotation

--- src/main/java/com/PPKWU/ZAD1/StringReverser.java
+++ src/main/java/com/PPKWU/ZAD1/StringReverser.java
@@ -1,11 +1,12 @@
 package com.PPKWU.ZAD1;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class StringReverser {
     @RequestMapping(path = "/reverse")
-    public String Reverse (String entryText) { return new StringBuilder(entryText).reverse().toString();}
+    public String Reverse (@RequestParam(value = "input", required = false) String entryText) { return new StringBuilder(entryText).reverse().toString();}
 
 }
