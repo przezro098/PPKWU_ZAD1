20705d80e48d85262c186c2f619fc2ce4b455139
Tomasz Kucharczyk <215786@edu.p.lodz.pl>
2020-10-20 14:25:56 +0200

    Project initialization

---
+++ src/test/java/com/PPKWU/ZAD1/Zad1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.PPKWU.ZAD1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Zad1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
