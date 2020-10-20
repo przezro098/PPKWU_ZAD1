0022000395e3d8bc7b619a86da28bc9dd1ef4806
Tomasz Kucharczyk <215786@edu.p.lodz.pl>
2020-10-20 14:38:18 +0200

    Added RequestMapping annotation

--- src/main/java/com/PPKWU/ZAD1/StringReverser.java
+++ src/main/java/com/PPKWU/ZAD1/StringReverser.java
@@ -1,10 +1,11 @@
 package com.PPKWU.ZAD1;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class StringReverser {
-
+    @RequestMapping(path = "/reverse")
     public String Reverse (String entryText) { return new StringBuilder(entryText).reverse().toString();}
 
 }
